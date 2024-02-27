9ebc02221ab10b5bf2c224d6273729c845a79650
felix1251 <domhack123@gmail.com>
2024-02-28 00:26:48 +0800

    sidebar menu item component url prop

--- src/components/molecules/SidebarMenuItem.tsx
+++ src/components/molecules/SidebarMenuItem.tsx
@@ -1,23 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface ISidebarMenusProps {
   icon: React.ReactNode;
   name: string;
+  url?: string;
   active?: boolean;
 }
 
 const SidebarMenus: React.FunctionComponent<ISidebarMenusProps> = ({
   icon,
   name,
+  url = "/",
   active = false,
 }: ISidebarMenusProps) => {
   return (
-    <div
+    <Link
+      to={url}
       className={`px-4 py-2.5 text-lg font-medium border-l-[6px] flex gap-4 items-center hover:bg-gray-100 ${active ? "border-primary text-primary" : "border-white text-secondary/70 hover:border-gray-100"}`}
     >
       {icon}
       {name}
-    </div>
+    </Link>
   );
 };
 
