53aee5891e0a15a6889ef0a0a3d0eb4e0233e27c
felix1251 <domhack123@gmail.com>
2024-02-28 00:18:29 +0800

    adjust sidebar color

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -9,11 +9,11 @@ const Sidebar: React.FunctionComponent = () => {
     <div className="sticky top-0 h-screen min-w-72 bg-white border-r">
       <div className="p-5 flex flex-col justify-start items-start">
         <CompanyLogo />
-        <div className="text-secondary font-bold text-lg -mt-2 ml-9">
+        <div className="text-secondary/80 font-bold text-lg -mt-2 ml-9">
           Merchant
         </div>
       </div>
-      <div className="flex flex-col mt-2">
+      <div className="flex flex-col">
         <SidebarMenuItem
           name="Items"
           icon={<IoBagHandle className="text-2xl" />}
