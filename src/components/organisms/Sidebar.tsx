856d573d6f6d93f73634cd5688dd7d6b942f5c5d
felix1251 <domhack123@gmail.com>
2024-02-27 23:31:46 +0800

    sidebar company logo

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,9 +1,14 @@
+import { CompanyLogo } from "@/atoms";
 import React from "react";
 
 const Sidebar: React.FunctionComponent = () => {
   return (
-    <div className="sticky top-0 h-screen min-w-72 bg-white border-r drop-shadow-md p-5">
-      Sidebar
+    <div className="sticky top-0 h-screen min-w-72 bg-white border-r drop-shadow-md">
+      <div className="p-5 flex flex-col justify-start items-start">
+        <CompanyLogo />
+        <div className="text-primary font-semibold -mt-2 ml-7">Merchant</div>
+      </div>
+      <div></div>
     </div>
   );
 };
