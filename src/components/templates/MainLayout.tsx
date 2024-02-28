8aeaf43a5d91703c68d9f0928a4f7d6842eedc43
felix1251 <domhack123@gmail.com>
2024-02-29 03:45:18 +0800

    responsive ui for all components

--- src/components/templates/MainLayout.tsx
+++ src/components/templates/MainLayout.tsx
@@ -9,7 +9,7 @@ const MainLayout: React.FunctionComponent = () => {
       <Toaster />
       <div className="w-full flex flex-col-reverse md:flex-row rounded-2xl">
         <Sidebar />
-        <div className="flex flex-col w-full mb-12 md:mb-0">
+        <div className="flex flex-col w-full mb-20 md:mb-0">
           <Navbar />
           <main className="margin-max-screen pb-4 pt-6">
             <Outlet />
