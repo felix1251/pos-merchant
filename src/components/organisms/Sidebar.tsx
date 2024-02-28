1bbd395db0d2dd8cf41dd24a83a00507e9312fbb
felix1251 <domhack123@gmail.com>
2024-02-29 05:49:37 +0800

    fix sidebar menu list padding in lg screen

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -6,14 +6,14 @@ import { IoBagCheck, IoBagHandle } from "react-icons/io5";
 
 const Sidebar: React.FunctionComponent = () => {
   return (
-    <div className="fixed bottom-0 min-w-full border-r bg-white md:sticky md:top-0 md:h-screen md:min-w-72">
+    <div className="fixed bottom-0 min-w-full border-r bg-white md:sticky md:top-0 md:h-screen lg:min-w-72">
       <div className="hidden flex-col items-start justify-start p-5 md:flex">
         <CompanyLogo />
         <div className="-mt-2 ml-9 text-lg font-bold text-secondary/80">
           Merchant
         </div>
       </div>
-      <div className="flex justify-between border-t-2 px-4 py-2 md:flex-col md:justify-start md:border-none lg:p-0">
+      <div className="flex justify-between border-t-2 px-4 py-2 md:flex-col md:justify-start md:border-none md:p-0">
         <SidebarMenuItem
           name="Items"
           icon={<IoBagHandle className="text-3xl sm:text-2xl" />}
